1cb6048efeeaf76c42cd189186c2302f9f0721f6
speschellk <kaychristensen@gmail.com>
2017-01-05 17:35:45 -0800

    Use Parse song library

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,20 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
+
+  initialize: function() {
+    var context = this;
+
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/songs/',
+      type: 'GET',
+      data: {'keys': 'artist,title,url'},
+      contentType: 'application/json',
+      success: function(data) {
+        context.add(data.results);
+      }
+    });
+  },
 
 });
